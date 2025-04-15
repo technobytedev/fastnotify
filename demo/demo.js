4564dcb307a3e07fedbba867ddd999842b5824da
Cedric D. Isubol <64195741+technobytedev@users.noreply.github.com>
2025-04-16 07:42:39 +0800

    Add files via upload

--- demo/demo.js
+++ demo/demo.js
@@ -1,16 +1,10 @@
 import { notify } from '../src/fastnotify.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-  const triggerButton = document.querySelector('#trigger');
-
-  if (triggerButton) {
-    triggerButton.addEventListener('click', () => {
-      notify({
-        message: 'This is a success notification!',
-        type: 'success',
-        duration: 3000,
-        position: 'top-right',
-      });
-    });
-  }
-});
+window.trigger = () => {
+  notify({
+    message: 'This is a success notification!',
+    type: 'success',
+    duration: 3000,
+    position: 'top-right'
+  });
+};
